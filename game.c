1c038f34b53401e8feb2fc53a12e80ac5bd46818
gracewanggw <grace.wang.24@dartmouth.edu>
2021-05-26 21:56:08 -0400

    game complete

--- game.c
+++ game.c
@@ -8,26 +8,74 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* ********** function prototypes ********* */
 static int isnewline(int c);
 static int never(int c);
 char* file_readLine(FILE* fp);
 char* file_readUntil(FILE* fp, int (*stopfunc)(int c));
+static void printIntro();
+static void printChoice(int idx);
+static void promptUser();
 
+
+
+/* ********* main ********** */
 int main()
 {
-    int idx = 5;
+    printIntro();
+
+    int idx = 6;
     char* input = NULL;
+    printChoice(idx);
+    idx--;
+    promptUser();
     while ((input = file_readLine(stdin)) != NULL) {
-        if (strcmp(input,"n") == 0) {
-            fprintf(stdout, "Congratulations, you learned your lesson\n");
-            return 0;
-        } else if (idx == 0) {
-            fprintf(stdout, "GAME OVER \n Moral of the story: ");
-            return 0;
-        }
+      fprintf(stdout, "\n");
+      if (strcmp(input,"no") == 0) {
+          fprintf(stdout, "Congratulations, you learned your lesson\n");
+          return 0;
+      } else if (idx == 0) {
+          fprintf(stdout, "You are back in your hovel \nMoral of the story:\nbe grateful for what you have. Greediness doesn't get you anywhere.");
+          return 0;
+      } else if (strcmp(input,"yes") == 0) {
+        printChoice(idx);
         idx--;
+      } 
+      promptUser();
     }
     return 1;
+}
+
+static void 
+promptUser()
+{
+  fprintf(stdout, "(yes or no): ");
+}
+static void
+printIntro()
+{
+    fprintf(stdout, "Intro: \n");
+    fprintf(stdout, "A magical flounder is able to grant you anything you wish for\n");
+    fprintf(stdout, "It already granted you your wish to turn your hovel into a cottage\n");
+
+}
+
+static void
+printChoice(int idx)
+{
+    if (idx == 6) {
+      fprintf(stdout, "Now that you have your cottage, do you want to ask the flounder for a castle?\n");
+    } else if (idx == 5) {
+      fprintf(stdout, "Now that you have your castle, do you want to be king?\n");
+    } else if (idx == 4) {
+      fprintf(stdout, "Now that you are king, do you want to be emperor?\n");
+    } else if (idx == 3) {
+      fprintf(stdout, "Now that you are emperor, do you you want to be pope?\n");
+    } else if (idx == 2) {
+      fprintf(stdout, "Now that you are pope, you can't become anything greater...do you want to continue?\n");
+    } else if (idx == 1) {
+      fprintf(stdout, "The only thing greater is God...do you want to be like God?\n");
+    }
 }
 
 static int isnewline(int c) { return (c == '\n'); }
